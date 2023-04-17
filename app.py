e3d27c92b664df13bb682d54f1566b7d7aeb1547
Madeleine Dobbie <madeleinedobbie@Madeleines-MacBook-Pro.local>
2023-04-17 15:42:40 +1000

    more goal fixes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import json
 
 from os import path
 from backend.config import Config
-from backend.model import Goal, User
+from backend.model import Goal, User, Question
 
 app = Flask(__name__,static_folder='backsupport/build',static_url_path='')
 
@@ -26,18 +26,17 @@ if not path.exists('./app.db'):
         db.session.commit()
     # chatbot tings
         print("im new max")
-        from backend.model import Question
-        # db.session.query(Question).delete()
-        # open file
-        # with open('./backend/chatbot.csv', 'r') as csvfile:
-        #     datareader = csv.reader(csvfile)
-        #     print(datareader)
-        #     for row in datareader:
-        #         print("thisss is row" + str(row))
-        #         q = Question(question = row[0], category = row[1], response1 = row[2], response2 = row[3], response3 = row[4], response4 = row[5], rGood = row[6], rBad = row[7], rEnd = row[8], rChange = row[9], rGoal = row[10], cGood = row[11], cBad = row[12], cChange = row[13], cEnd = row[14], cGoal = row[15])
-        #         db.session.add(q)
-        #         print(q)
-        #         db.session.commit()
+        db.session.query(Question).delete()
+        open file
+        with open('./backend/chatbot.csv', 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            print(datareader)
+            for row in datareader:
+                print("thisss is row" + str(row))
+                q = Question(question = row[0], category = row[1], response1 = row[2], response2 = row[3], response3 = row[4], response4 = row[5], rGood = row[6], rBad = row[7], rEnd = row[8], rChange = row[9], rGoal = row[10], cGood = row[11], cBad = row[12], cChange = row[13], cEnd = row[14], cGoal = row[15])
+                db.session.add(q)
+                print(q)
+                db.session.commit()
 
 @app.route('/')
 def serve():
